4d133dece61f832506d2bc87a6fccf7384ddf6b8
Nícolas de Assis <nicolasassis@ciandt.com>
2024-04-28 23:55:23 -0300

    js changes

--- JS/sobre.js
+++ JS/sobre.js
@@ -1,30 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.sobre');
+
+    function showCards() {
+        cards.forEach(card => {
+            if (isElementInViewport(card)) {
+                card.classList.add('show');
+            }
+        });
+    }
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    showCards();
+    window.addEventListener('scroll', showCards);
+
     document.addEventListener('keypress', function(event) {
         if (event.key === 'h') {
             document.querySelector('nav a[href="index.html"]').click();
-        }
-    });
-
-    document.addEventListener('keypress', function(event) {
-        if (event.key === 'd') {
+        } else if (event.key === 'd') {
             document.querySelector('nav a[href="dicas.html"]').click();
-        }
-    });
-
-    document.addEventListener('keypress', function(event) {
-        if (event.key === 'o') {
+        } else if (event.key === 'o') {
             document.querySelector('nav a[href="oportunidades.html"]').click();
-        }
-    });
-
-    document.addEventListener('keypress', function(event) {
-        if (event.key === 's') {
+        } else if (event.key === 's') {
             document.querySelector('nav a[href="sobre.html"]').click();
-        }
-    });
-
-    document.addEventListener('keypress', function(event) {
-        if (event.key === 'f') {
+        } else if (event.key === 'f') {
             document.querySelector('nav a[href="forum.html"]').click();
         }
     });
